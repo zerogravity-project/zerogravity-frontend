5290935aeefa2c4bf218bc29f974ee6bfe5ba770
minukHwang <damin2003@yonsei.ac.kr>
2024-05-14 16:30:39 +0900

    [ZRGV-240514] init: Set up initial Vue project structure and ESLint

---
+++ zerogravity/.eslintrc.cjs
@@ -0,0 +1,32 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:vue/vue3-strongly-recommended',
+    'eslint:recommended',
+    // '@vue/eslint-config-prettier/skip-formatting'
+    // 'prettier',
+    // 'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'semi': ['error', 'never'],
+    'quotes': ['error', 'single'],
+    'max-len': ['error', { 'code': 100 }],
+    'comma-dangle': ['error', 'always-multiline'],
+    'no-tabs': 'error',
+    'no-multiple-empty-lines': ['error', { 'max': 1, 'maxEOF': 1, 'maxBOF': 0 }],
+    'padded-blocks': ['error', { 'blocks': 'never', 'classes': 'never', 'switches': 'never' }],
+    'no-trailing-spaces': 'error',
+    'no-console': 'warn',
+    'vue/script-indent': ['error', 2, {
+      'baseIndent': 1,
+      'switchCase': 1,
+      'ignores': [],
+    }],
+  },
+}
