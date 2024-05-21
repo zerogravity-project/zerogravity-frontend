33e3bea5f1ae29e66906f74542c9419cd121c323
celestedayoung <dayoungpyo@gmail.com>
2024-05-21 09:43:21 +0900

    [ZRGV-240521] feat: Add conditional rendering for drawer

--- zerogravity/src/AppTest.vue
+++ zerogravity/src/AppTest.vue
@@ -1,10 +1,26 @@
 <script setup>
-import DisplayClock from './components/clock/DisplayClock.vue';
+  import { RouterView, useRoute } from 'vue-router'
+  import DrawerContainer from './components/drawer/DrawerContainer.vue'
+  import { computed } from 'vue'
+
+  const route = useRoute()
+
+  const isDrawerVisible = computed(() => {
+    const hideRoutes = ['/', '/spaceout']
+    return !hideRoutes.includes(route.path)
+  })
+
 </script>
 
 <template>
-  <DisplayClock />
+  <DrawerContainer
+    v-if="isDrawerVisible"
+    class="menu"
+    variant="menu"
+    :width="'18.75rem'"
+    :is-right="false"
+  />
+  <RouterView />
 </template>
 
-<style lang="scss" scoped>
-</style>
+<style scoped></style>
